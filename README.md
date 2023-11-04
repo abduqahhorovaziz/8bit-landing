8e418ef891a266df60ede15d2025a8d062304031
Aziz Muradov <aziz.abduqahhorov6111@gmail.com>
2023-11-04 11:16:44 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# 8BIT Landing page
+
+A simple web project built with plain HTML, CSS, and JavaScript.
+
+## Overview
+
+This project is a basic web application developed using HTML, CSS, and JavaScript. It doesn't require any server-side setup, making it easy to run locally on your machine.
+
+## Prerequisites
+
+Before you get started, ensure you have the following tools installed:
+
+- [Git](https://git-scm.com/)
+- A modern web browser (e.g., Chrome, Firefox, or Edge)
+
+## Getting Started
+
+To run this project on your local machine, follow these steps:
+
+1. Clone the repository to your local system using Git. Open your terminal and run:
+
+   ```bash
+   git clone https://github.com/yourusername/your-repo.git
---
+++ js/main.js
@@ -0,0 +1,52 @@
+const link = document.querySelectorAll(".link");
+const hoverReveal = document.querySelectorAll(".hover-reveal");
+const linkImages = document.querySelectorAll(".hidden-img");
+
+for (let i = 0; i < link.length; i++) {
+  link[i].addEventListener("mousemove", (e) => {
+    hoverReveal[i].style.opacity = 1;
+    hoverReveal[i].style.transform = `translate(-100%, -50%) rotate(5deg)`;
+    linkImages[i].style.transform = "scale(1, 1)";
+    hoverReveal[i].style.left = e.clientX + "px";
+  });
+  link[i].addEventListener("mouseleave", (e) => {
+    hoverReveal[i].style.opacity = 0;
+    hoverReveal[i].style.transform = `translate(-50%, -50%) rotate(-5deg)`;
+    linkImages[i].style.transform = "scale(0.8, 0.8)";
+  });
+}
+
+const navMenu = document.querySelector(".navbar__menu");
+const btnBurger = document.getElementById("nav-toggle");
+const iconBurger = document.getElementById("nav-burger");
+const iconClose = document.getElementById("nav-close");
+const navLink = document.querySelectorAll(".navbar__link");
+
+btnBurger.addEventListener("click", () => {
+  if (navMenu.classList.contains("navbar__menu-hidden")) {
+    navMenu.classList.remove("navbar__menu-hidden");
+    navMenu.classList.add("navbar__menu-active");
+    // document.body.classList.add("no-scroll");
+    iconBurger.classList.add("opacity-none");
+    iconClose.classList.remove("opacity-none");
+  } else {
+    navMenu.classList.remove("navbar__menu-active");
+    navMenu.classList.add("navbar__menu-hidden");
+    // document.body.classList.remove("no-scroll");
+    iconBurger.classList.remove("opacity-none");
+    iconClose.classList.add("opacity-none");
+  }
+});
+
+const closeNavMenu = () => {
+  if (navMenu.classList.contains("navbar__menu-active")) {
+    navMenu.classList.remove("navbar__menu-active");
+    navMenu.classList.add("navbar__menu-hidden");
+    iconBurger.classList.remove("opacity-none");
+    iconClose.classList.add("opacity-none");
+  }
+};
+
+navLink.forEach((link) => {
+  link.addEventListener("click", closeNavMenu);
+});
